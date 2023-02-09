382562ca047afab196e48421fa02a2172dd95e33
AlexMDC <>
2023-02-09 17:07:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+class A{
+  int i;
+  A(int i){ // constructor to pass in i's value
+    this.i = i;
+  }
+  A(){ // to make a generic A with i as 100
+    this.i = 100;
+  }
+  public void setI(int i){
+    this.i = i; // we can change the i after the constructor
+  }
+  public String toString(){ //to display info
+    return "A i=" + i;
+  }
+}
+
+
+class Main {
+  public static void main(String[] args) {
+    A a = new A();
+    System.out.println(a); //what are we expecting?
+    A b = a;
+    System.out.println(b);
+    b.setI(50);
+    System.out.println(a); // Will print out new value of 50 even though you are calling for a. Working off the same class so the value is changing due to method setI.
+  }
+}
